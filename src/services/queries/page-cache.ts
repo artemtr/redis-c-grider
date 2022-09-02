767f5e6e28e6feb814a7be60d8bcdbcfa1f14712
Artem Travlo <info.travlo@gmail.com>
2022-09-02 16:49:29 +0300

    Go to 4 folder

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,15 +1,20 @@
 import { client } from '$services/redis';
 const cachedRoutes = ['./about', '/privacy', '/auth/signin', '/auth/signup'];
+
+function pageKey(route: string): string {
+	return 'pagecache#' + route;
+}
 export const getCachedPage = (route: string) => {
 	if (cachedRoutes.includes(route)) {
-		return client.get('pagecache#' + route);
+		return client.get(pageKey(route));
 	}
 	return null;
 };
 
 export const setCachedPage = (route: string, page: string) => {
 	if (cachedRoutes.includes(route)) {
-		return client.set('pagecache#' + route, page, {
+		// ts-ignore
+		client.set(pageKey(route), page, {
 			EX: 2
 		});
 	}
